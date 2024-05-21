f689ce6e17a71ce44a9726cd36dc761d5f092015
dr-jeky11 <153104535+dr-jeky11@users.noreply.github.com>
2024-05-21 18:02:10 +0000

    Deploying to gh-pages from @ dr-jeky11/goit-js-hw-10@f59f1d33dff795df8b033993af5fa613f9bab56b 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as s}from"./assets/vendor-77e16229.js";const i=document.querySelector(".form");i.addEventListener("submit",m=>{m.preventDefault();const o=new FormData(i),t=o.get("delay"),r=o.get("state");new Promise((e,n)=>{setTimeout(()=>{r==="fulfilled"?e(t):n(t)},t)}).then(e=>s.success({message:`✅Fulfilled promise in ${e}ms`,position:"topRight",icon:""})).catch(e=>s.error({message:`❌ Rejected promise in ${e}ms`,position:"topRight",icon:""}))});
+//# sourceMappingURL=commonHelpers2.js.map
